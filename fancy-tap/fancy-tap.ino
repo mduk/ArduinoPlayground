608ca4899443dd7a9a76ca923cab74a812503b7c
Daniel Kendell <daniel.kendell@gmail.com>
2019-05-22 09:22:35 +0100

    fancy-tap

--- fancy-tap/fancy-tap.ino
+++ fancy-tap/fancy-tap.ino
@@ -1,39 +1,108 @@
-#define VALVE_PIN          3
+#include <Keypad.h>
+
+#define VALVE_PIN          13
+
+#define STATUS_RED_PIN     11
+#define STATUS_GRN_PIN     12
+
 #define SENSOR_PIN         2
 #define SENSOR_INTERRUPT   0
 #define CALIBRATION_FACTOR 4.5
 
-volatile byte pulseCount;
+#define KEYPAD_ROWS        4
+#define KEYPAD_COLS        4
 
-float flowRate;
-unsigned int flowMilliLitres;
-unsigned long totalMilliLitres;
+bool do_sensor = true;
 
-unsigned long oldTime;
+const char  keymap[KEYPAD_ROWS][KEYPAD_COLS] = {
+  { '1', '2', '3', 'A' },
+  { '4', '5', '6', 'B' },
+  { '7', '8', '9', 'C' },
+  { '*', '0', '#', 'D' }
+};
 
-void setup()
-{
-  Serial.begin(38400);
+const char  kp_row_pins[KEYPAD_ROWS] = { 7, 8, 9, 10 };
+const char  kp_col_pins[KEYPAD_COLS] = { 3, 4, 5, 6  };
 
-  pinMode(SENSOR_PIN, INPUT);
-  digitalWrite(SENSOR_PIN, HIGH);
+// Here be dragons
+
+#define STATE_CLOSED 0
+#define STATE_OPEN   1
+
+uint8_t state = 0;
+
+Keypad kp = Keypad(makeKeymap(keymap),
+                   kp_row_pins, kp_col_pins,
+                   KEYPAD_ROWS, KEYPAD_COLS);
+
+volatile byte pulseCount = 0;
+
+float flowRate = 0.0;
+unsigned int flowMilliLitres = 0;
+unsigned long totalMilliLitres = 0;
+
+unsigned long oldTime = 0;
+
+unsigned long dispense_ml = 0;
+
+void set_state(uint8_t new_state) {
+  Serial.print("set_state: ");
+  Serial.print(state);
+  Serial.print(" => ");
+  Serial.println(new_state);
+
+  switch (new_state) {
+    case STATE_OPEN:
+      Serial.println("Opening Valve");
+      digitalWrite(STATUS_RED_PIN, LOW);
+      digitalWrite(STATUS_GRN_PIN, HIGH);
+      digitalWrite(VALVE_PIN, HIGH);
+      break;
+
+    case STATE_CLOSED:
+      Serial.println("Closing Valve. Awaiting Input.");
+      digitalWrite(STATUS_RED_PIN, HIGH);
+      digitalWrite(STATUS_GRN_PIN, LOW);
+      digitalWrite(VALVE_PIN, LOW);
+      dispense_ml = 0;
+      totalMilliLitres = 0;
+      break;
+  }
+
+  state = new_state;
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(STATUS_RED_PIN, OUTPUT);
+  digitalWrite(STATUS_RED_PIN, LOW);
+
+  pinMode(STATUS_GRN_PIN, OUTPUT);
+  digitalWrite(STATUS_GRN_PIN, LOW);
 
   pinMode(VALVE_PIN, OUTPUT);
   digitalWrite(VALVE_PIN, LOW);
 
-  pulseCount        = 0;
-  flowRate          = 0.0;
-  flowMilliLitres   = 0;
-  totalMilliLitres  = 0;
-  oldTime           = 0;
+  if (do_sensor) {
+    pinMode(SENSOR_PIN, INPUT);
+    digitalWrite(SENSOR_PIN, HIGH);
 
-  attachInterrupt(SENSOR_INTERRUPT, pulseCounter, FALLING);
+    attachInterrupt(SENSOR_INTERRUPT, pulseCounter, FALLING);
+  }
+
+  set_state(STATE_CLOSED);
 }
 
-void loop()
-{
-  if((millis() - oldTime) > 1000)    // Only process counters once per second
-  {
+void loop() {
+  switch (state) {
+    case STATE_OPEN: loop_open(); break;
+    case STATE_CLOSED: loop_closed(); break;
+  }
+}
+
+void loop_open() {
+  if(do_sensor && (millis() - oldTime) > 1000) { // Only process counters once per second
     detachInterrupt(SENSOR_INTERRUPT);
 
     // The hall-effect flow sensor outputs approximately 4.5 pulses per second per
@@ -86,33 +155,65 @@ void loop()
     attachInterrupt(SENSOR_INTERRUPT, pulseCounter, FALLING);
   }
 
-  if (Serial.available()) {
-    char c = Serial.read();
-    switch (c) {
-      case ' ':
-        open();
+  if (dispense_ml > 0 && totalMilliLitres > dispense_ml) {
+    set_state(STATE_CLOSED);
+    return;
+  }
+
+  char key = kp.getKey();
+  if (key != NO_KEY) {
+    set_state(STATE_CLOSED);
+  }
+}
+
+void loop_closed() {
+  char key = kp.getKey();
+  if (key != NO_KEY) {
+    Serial.println();
+    Serial.print("Key Pressed: ");
+    Serial.println(key);
+
+    switch (key) {
+
+      case '*':
+        dispense_ml = 0;
         totalMilliLitres = 0;
         break;
 
-      case 'o': open(); break;
-      case 'c': close(); break;
+      case '#':
+        set_state(STATE_OPEN);
+        break;
+
+      case 'A':
+        dispense_ml = 10000;
+        totalMilliLitres = 0;
+        set_state(STATE_OPEN);
+        break;
+
+      case 'B':
+        dispense_ml = 70000;
+        totalMilliLitres = 0;
+        set_state(STATE_OPEN);
+        break;
+
+      case 'C':
+      case 'D':
+        Serial.print("Key has no function: ");
+        Serial.println(key);
+        break;
+
+      default:
+        if (state == STATE_CLOSED) {
+          dispense_ml = (dispense_ml * 10) + (key - '0');
+          Serial.print("dispense_ml: ");
+          Serial.println(dispense_ml);
+        }
+        break;
+
     }
   }
-
-  if (totalMilliLitres > 2000) {
-    digitalWrite(VALVE_PIN, LOW);
-  }
 }
 
-void pulseCounter()
-{
+void pulseCounter() {
   pulseCount++;
-}
-
-void open() {
-  digitalWrite(VALVE_PIN, HIGH);
-}
-
-void close() {
-  digitalWrite(VALVE_PIN, LOW);
 }
