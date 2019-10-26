ab7ea703122de8d23440f783f2e805ba4bd6216b
Daniel Kendell <daniel.kendell@gmail.com>
2019-10-26 15:26:06 +0100

    baud

--- WemosDallasTemp/WemosDallasTemp.ino
+++ WemosDallasTemp/WemosDallasTemp.ino
@@ -8,7 +8,7 @@
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 OneWire oneWire(ONE_WIRE_BUS);
 
-// Pass our oneWire reference to Dallas Temperature. 
+// Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
 
 /*
@@ -17,7 +17,7 @@ DallasTemperature sensors(&oneWire);
 void setup(void)
 {
   // start serial port
-  Serial.begin(9600);
+  Serial.begin(115200);
   Serial.println("Dallas Temperature IC Control Library Demo");
 
   // Start up the library
@@ -29,7 +29,7 @@ void setup(void)
  * Main function, get and show the temperature
  */
 void loop(void)
-{ 
+{
   sensors.requestTemperatures();
-  Serial.println(sensors.getTempCByIndex(0));  
+  Serial.println(sensors.getTempCByIndex(0));
 }
