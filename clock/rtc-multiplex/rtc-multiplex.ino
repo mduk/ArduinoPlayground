5b80bdf1ce617a41ad75b647c804ed5f598e9c08
Daniel Kendell <daniel.kendell@gmail.com>
2021-03-26 20:07:16 +0000

    character masking

--- clock/rtc-multiplex/rtc-multiplex.ino
+++ clock/rtc-multiplex/rtc-multiplex.ino
@@ -17,17 +17,17 @@ int segmentF = 7;
 int segmentG = 4;
 
 byte characters[] = {
-//A B C D E F G
-  1,1,1,1,1,1,0, // 0
-  0,1,1,0,0,0,0, // 1
-  1,1,0,1,1,0,1, // 2
-  1,1,1,1,0,0,1, // 3
-  0,1,1,0,0,1,1, // 4
-  1,0,1,1,0,1,1, // 5
-  1,0,1,1,1,1,1, // 6
-  1,1,1,0,0,0,0, // 7
-  1,1,1,1,1,1,1, // 8
-  1,1,1,1,0,1,1, // 9
+//   ABCDEFG
+  0b01111110, // 0
+  0b00110000, // 1
+  0b01101101, // 2
+  0b01111001, // 3
+  0b00110011, // 4
+  0b01011011, // 5
+  0b01011111, // 6
+  0b01110000, // 7
+  0b01111111, // 8
+  0b01111011, // 9
 };
 
 int last_hour;
@@ -82,10 +82,10 @@ void loop() {
   int minutes_units = now_minute % 10;
 
   switch (displayDigit) {
-    case 1: setDigit(1); setSegments(hours_tens); break;
-    case 2: setDigit(2); setSegments(hours_units); break;
-    case 3: setDigit(3); setSegments(minutes_tens); break;
-    case 4: setDigit(4); setSegments(minutes_units); break;
+    case 1: setDigit(1); setSegments(charSegments(hours_tens   )); break;
+    case 2: setDigit(2); setSegments(charSegments(hours_units  )); break;
+    case 3: setDigit(3); setSegments(charSegments(minutes_tens )); break;
+    case 4: setDigit(4); setSegments(charSegments(minutes_units)); break;
   }
 
   displayDigit++;
@@ -98,6 +98,29 @@ void loop() {
   last_hour = now_hour;
   last_minute = now_minute;
   last_second = now_second;
+}
+
+byte mask(int step, byte segments) {
+  byte mask = 0;
+
+  switch (step) {
+    default: mask = 0b11111111; break;
+    case 55: mask = 0b10111111; break;
+    case 56: mask = 0b10011101; break;
+    case 57: mask = 0b10011100; break;
+    case 58: mask = 0b10001000; break;
+    case 59: mask = 0b10000000; break;
+  }
+
+  return segments & mask;
+}
+
+byte maskOff(byte from) {
+  return from & 0b01100010;
+}
+
+byte charSegments(int value) {
+  return characters[value];
 }
 
 void setDigit(int digit) {
@@ -132,20 +155,13 @@ void setDigit(int digit) {
   }
 }
 
-void setSegments(int value) {
-  int index = value;
-
-  if (value > 0) {
-    index = value * 7;
-  }
-
-  digitalWrite(segmentA, !characters[index]);
-  digitalWrite(segmentB, !characters[index + 1]);
-  digitalWrite(segmentC, !characters[index + 2]);
-  digitalWrite(segmentD, !characters[index + 3]);
-  digitalWrite(segmentE, !characters[index + 4]);
-  digitalWrite(segmentF, !characters[index + 5]);
-  digitalWrite(segmentG, !characters[index + 6]);
+void setSegments(byte segments) {
+  digitalWrite(segmentA, !((segments >> 6 ) & 1));
+  digitalWrite(segmentB, !((segments >> 5 ) & 1));
+  digitalWrite(segmentC, !((segments >> 4 ) & 1));
+  digitalWrite(segmentD, !((segments >> 3 ) & 1));
+  digitalWrite(segmentE, !((segments >> 2 ) & 1));
+  digitalWrite(segmentF, !((segments >> 1 ) & 1));
+  digitalWrite(segmentG, !((segments      ) & 1));
 }
-
 
